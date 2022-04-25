ac526587cccb5a07ce8ffd744c2356e150635878
CaDiBob <nikolay.v.byankin@gmail.com>
2022-04-25 21:28:03 +0900

    Добавил модел Places

--- places/models.py
+++ places/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-print('Привет!')
+class Places(models.Model):
+    title = models.CharField('Название', max_length=100)
+    description_short = models.CharField('Короткое описание', max_length=250)
+    description_long = models.TextField('Описание')
+    lng = models.FloatField('Долгота')
+    lat = models.FloatField('Широта')
+
+    def __str__(self):
+        return self.title
