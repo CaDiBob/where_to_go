2d555fa7811d494d2a76672e8dda19e492a625c0
CaDiBob <nikolay.v.byankin@gmail.com>
2022-04-28 20:02:43 +0900

    Настроил MEDIA

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path
+from django.urls import re_path
+from django.views.static import serve
+
 from where_to_go import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
