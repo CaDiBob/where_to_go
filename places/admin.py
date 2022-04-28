efc0b90302ba9fc9f36d64f619cab8b9bfd9e0de
CaDiBob <nikolay.v.byankin@gmail.com>
2022-04-28 19:26:55 +0900

    Добавил модель для картинок

--- places/admin.py
+++ places/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from places.models import Places
+from places.models import Places, Image
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('object_id', 'places')
+    list_display_links = ('places',)
+    ordering = ('object_id',)
 
 admin.site.register(Places)
--- places/models.py
+++ places/models.py
@@ -1,4 +1,7 @@
+from statistics import mode
+from tabnanny import verbose
 from django.db import models
+
 
 class Places(models.Model):
     title = models.CharField('Название', max_length=100)
@@ -7,5 +10,24 @@ class Places(models.Model):
     lng = models.FloatField('Долгота')
     lat = models.FloatField('Широта')
 
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
+
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    places = models.ForeignKey(Places,
+                               verbose_name='Места',
+                               on_delete=models.CASCADE)
+    image = models.ImageField('Картинка')
+    object_id = models.PositiveIntegerField(blank=True, default=0)
+
+    class Meta:
+        verbose_name = 'Картинка'
+        verbose_name_plural = 'Картинки'
+
+    def __str__(self):
+        return self.places.title
