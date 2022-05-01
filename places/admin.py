8f04b2213323ab26837d23d91e2589abff7e04c1
CaDiBob <nikolay.v.byankin@gmail.com>
2022-05-01 18:50:48 +0900

    Добавил inlines на картинки в модель Places

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from places.models import Places, Image
 
+
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('object_id', 'places')
-    list_display_links = ('places',)
-    ordering = ('object_id',)
+    list_display_links = ('places', )
+    ordering = ('object_id', )
 
-admin.site.register(Places)
+
+@admin.register(Places)
+class PlacesAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
