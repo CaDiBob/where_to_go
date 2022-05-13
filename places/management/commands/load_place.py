b603cf24a57d5c1278b2b796fc9b2c683a5c65da
CaDiBob <nikolay.v.byankin@gmail.com>
2022-05-13 18:38:19 +0900

    Добавил проверку исключений

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -7,6 +7,37 @@ from django.core.management.base import BaseCommand
 from places.models import Image, Place
 
 
+def dowload_json(json_url):
+    response = requests.get(json_url)
+    response.raise_for_status()
+    with open('place.json', 'w') as file:
+        file.write(response.text)
+
+
+def save_to_db():
+    with open('place.json', 'r') as file:
+        place = json.load(file)
+    new_place = Place(
+        title=place['title'],
+        description_short=place['description_short'],
+        description_long=place['description_long'],
+        lng=place['coordinates']['lng'],
+        lat=place['coordinates']['lat'],
+    )
+    new_place.save()
+
+    for number, img in enumerate(place['imgs'], 1):
+        response = requests.get(img)
+        response.raise_for_status()
+        image = response.content
+        image = Image()
+        image.image.save(f'image{number}.jpeg',
+                         ContentFile(response.content),
+                         save=False)
+        image.place = new_place
+        image.save()
+
+
 class Command(BaseCommand):
 
     help = 'Testing'
@@ -14,41 +45,12 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('url', nargs='?', type=str)
 
-    def dowload_json(self, json_url):
-        response = requests.get(json_url)
-        response.raise_for_status()
-        with open('place.json', 'w') as file:
-            file.write(response.text)
-
     def handle(self, *args, **options):
-
-        self.dowload_json(options['url'])
-
-        with open('place.json', 'r') as file:
-            place = json.load(file)
-        new_place = Place(
-            title=place['title'],
-            description_short=place['description_short'],
-            description_long=place['description_long'],
-            lng=place['coordinates']['lng'],
-            lat=place['coordinates']['lat'],
-        )
-        new_place.save()
-
-        for number, img in enumerate(place['imgs'], 1):
-            try:
-                response = requests.get(img)
-                response.raise_for_status()
-                image = response.content
-                image = Image()
-                image.image.save(f'image{number}.jpeg',
-                                 ContentFile(response.content),
-                                 save=False)
-                image.place = new_place
-                image.save()
-            except Exception:
-                continue
-
-        os.remove('place.json')
-
-        print('Локация успешно добавлена.')
+        try:
+            dowload_json(options['url'])
+            save_to_db()
+            print('Локация успешно добавлена.')
+        except Exception:
+            print('Что-то пошло не так! Проверте ссылку и повторите.')
+        finally:
+            os.remove('place.json')
